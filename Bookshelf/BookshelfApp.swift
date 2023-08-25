f6c42f188653768efbb515c621869885649795db
Jayson Brennan <jaysonbrennan@protonmail.com>
2023-08-25 13:26:51 -0400

    Initial Commit

---
+++ Bookshelf/BookshelfApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookshelfApp.swift
+//  Bookshelf
+//
+//  Created by Jayson Brennan on 8/25/23.
+//
+
+import SwiftUI
+
+@main
+struct BookshelfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
