d3ed8a3a4a409a038c177a87a60e9033fbfd1320
Jayson Brennan <jaysonbrennan@protonmail.com>
2023-08-25 15:49:47 -0400

    Adding tab bar

---
+++ Bookshelf/MainView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  Bookshelf
+//
+//  Created by Jayson Brennan on 8/25/23.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        TabView {
+            Text("Library")
+                .tabItem {
+                    Label("Library", systemImage: "building.columns.fill")
+                }
+            Text("Bookshelf")
+                .tabItem {
+                    Label("Bookshelf", systemImage: "books.vertical.fill")
+                }
+        }
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
